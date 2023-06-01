e74e4299cb8fbe22ec7ff933766cc20685380b2b
toxicface <47838537+toxicface@users.noreply.github.com>
2023-05-31 21:16:20 -0400

    sword

--- src/Sword.java
+++ src/Sword.java
@@ -1,5 +1,7 @@
 public class Sword extends AttackAbilities{
 
+
+    // sword
     Sword(int x, int y, int width, int height,  boolean isFriendly) {
         super(x, y, 100, 200, 30, isFriendly, 100);
     }
