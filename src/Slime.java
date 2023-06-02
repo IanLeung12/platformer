4adaa30565815def4a38dde944aead6a52e61c35
Razor177 <mishakay6@gmail.com>
2023-06-02 08:52:33 -0400

    moving slime perfect

--- src/Slime.java
+++ src/Slime.java
@@ -42,6 +42,7 @@ public class Slime extends Enemy{
         if (otherObject instanceof Spike) {
             //this.setHealth(-1);
         } else {
+            System.out.println(this + " has hit " + otherObject);
             double playerBottom = this.getY() + this.getHeight();
             double otherObjectTop = otherObject.getY();
             double playerRight = this.getX() + this.getWidth();
@@ -49,28 +50,37 @@ public class Slime extends Enemy{
             double playerLeft = this.getX();
             double colliderRight = otherObject.getX() + otherObject.getWidth();
 
-            if ((playerBottom > otherObjectTop) && ((this.getY() + this.getYSpeed()) < otherObjectTop)) { // top
+            if ((playerBottom > otherObjectTop) && ((this.getY() + this.getYSpeed()) < otherObjectTop)) { // top of moving
                 this.setLocation((int) this.getX(), (int) (otherObjectTop - this.getHeight()));
                 this.setYSpeed(0);
-            } else if (this.getY() < otherObjectTop + otherObject.getHeight() && playerBottom > otherObjectTop + otherObject.getHeight()) { // bottom
+                //System.out.println("enemy his top ");
+
+                double bottomRightX = (int) (this.getX() + this.getWidth());
+                double bottomRightY = (int) (this.getY() + this.getHeight());
+                double bottomLeftY = (int) (this.getY() + this.getHeight());
+
+                if ( ((otherObject.contains(bottomRightX, bottomRightY)) && (!otherObject.contains(this.getX(), bottomLeftY))) || ((!otherObject.contains(bottomRightX, bottomRightY)) && (otherObject.contains(this.getX(), bottomLeftY)))  )   {
+                    this.setXSpeed(this.getXSpeed() * (-1));
+                }
+
+
+            } else if (this.getY() < otherObjectTop + otherObject.getHeight() && playerBottom > otherObjectTop + otherObject.getHeight()) { // bottom of moving
+               // System.out.println("enemy hit bottom");
                 this.setLocation((int) this.getX(), (int) (otherObjectTop + otherObject.getHeight()));
                 this.setYSpeed(0);
-                System.out.println("enemy hit bottom");
 
-//                double bottomRightX = (int) (this.getX() + this.getWidth());
-//                double bottomRightY = (int) (this.getY() + this.getHealth());
-//                double bottomLeftY = (int) (this.getY() + this.getHeight());
-//
-//                if ( ((otherObject.contains(bottomRightX, bottomRightY)) && (!otherObject.contains(this.getX(), bottomLeftY))) || ((!otherObject.contains(bottomRightX, bottomRightY)) && (otherObject.contains(this.getX(), bottomLeftY)))  )   {
-//                    this.setXSpeed(this.getXSpeed() * (-1));
-//                }
 
 
 
             } else if (playerRight > colliderLeft && playerLeft < colliderLeft && playerBottom > otherObjectTop && this.getY() < otherObjectTop + otherObject.getHeight()) { // left
+                //System.out.println("enemy hit left");
+
                 this.setLocation((int) (colliderLeft - this.getWidth()), (int) this.getY());
                 this.setXSpeed(this.getXSpeed() * (-1));
+
             } else if (this.getX() < colliderRight && playerRight > colliderRight && playerBottom > otherObjectTop && this.getY() < otherObjectTop + otherObject.getHeight()) { // right
+               // System.out.println("enemy hit left");
+
                 this.setLocation((int) (colliderRight), (int) this.getY());
                 this.setXSpeed(this.getXSpeed() * (-1));
 
