7e8385efcc9ac57524db346fc4ebb6bd5d552711
Razor177 <mishakay6@gmail.com>
2023-06-02 22:52:57 -0400

    slime good and good movement

--- src/Constants.java
+++ src/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     static int maxXSpeed = 26;
     static int XSpeedAddition = 2;
     static int slimeVision = 300;
-    static int slimeSpeed = 10;
+    static int slimeSpeed = 2;
 
     public static int getSlimeSpeed() {
         return slimeSpeed;
--- src/Enemy.java
+++ src/Enemy.java
@@ -24,25 +24,140 @@ abstract public class Enemy extends Moveable {
     public abstract void move(Player player);
 
     public double distanceToPlayer(Player player) {
-//        double m, b;
-//
-//        m = ((player.getCenterY() - this.getCenterY()) / (player.getCenterX() - this.getCenterX()));
-//        b = ((this.getCenterY() / (m * this.getCenterX())));
-//
-//
-//
-//        this.get()
-//
+
+        boolean enemyVerticleExit = false;
+        boolean enemyHorizontalExit = false;
+
+        boolean playerVerticleExit = false;
+        boolean playerHorizontalExit = false;
+
+        double enemyYpointIntersection = 0;
+        double enemyXpointIntersection = 0;
+
+        double playerYpointIntersection = 0;
+        double playerXpointIntersection = 0;
+
+        boolean enemyRight = false;
+        boolean enemyLeft = false;
+        boolean enemyUp = false;
+        boolean enemyDown = false;
+
+        boolean playerRight = false;
+        boolean playerLeft = false;
+        boolean playerUp = false;
+        boolean playerDown = false;
+
+        double enemyY = 0;
+        double playerY = 0;
+
+        double m, b;
+
+        if (Math.abs(player.getCenterX()) - Math.abs(this.getCenterX()) == 0) {
+            m = Integer.MAX_VALUE;
+        } else {
+            m = ((player.getCenterY() - this.getCenterY()) / (player.getCenterX() - this.getCenterX()));
+        }
 
 
-        double xDistanceCenters = Math.abs(player.getCenterX() - this.getCenterX());
-        double yDistanceCenters = Math.abs(player.getCenterY() - this.getCenterY());
-        double centerDistance = Math.sqrt(Math.pow(xDistanceCenters, 2) + Math.pow(yDistanceCenters, 2)); // ==================================================================================================================================
+        b = ((this.getCenterY() - (m * this.getCenterX())));
 
-        double currentToCenter = (getWidth() < getHeight()) ? (centerDistance * (xDistanceCenters - getWidth()) / xDistanceCenters / 2) : (centerDistance * (yDistanceCenters - getHeight()) / yDistanceCenters / 2);
-        double playerToCenter = (getWidth() < getHeight()) ? (centerDistance * (xDistanceCenters - player.getWidth()) / xDistanceCenters / 2) : (centerDistance * (yDistanceCenters - player.getHeight()) / yDistanceCenters / 2);
+        enemyY = ((m * this.getX()) + b);
+        playerY = ((m * player.getX()) + b);
 
-        return currentToCenter + playerToCenter;
+
+
+        if ((enemyY <= this.getY() + this.getHeight()) && (enemyY >= this.getY())) {
+            enemyHorizontalExit = true;
+        } else {
+            enemyVerticleExit = true;
+        }
+
+        if ((playerY <= player.getY() + player.getHeight()) && (playerY >= player.getY())) {
+            playerHorizontalExit = true;
+        } else {
+            playerVerticleExit = true;
+        }
+
+
+
+
+        if (enemyHorizontalExit) {
+            if (player.getX() - this.getX() <= 0) {
+                enemyLeft = true;
+                playerRight = true;
+            } else {
+                enemyRight = true;
+                playerLeft = true;
+            }
+
+        } else if (enemyVerticleExit) {
+            if (player.getY() - this.getY() >= 0) {
+                enemyDown = true;
+                playerUp = true;
+            } else {
+                enemyUp = true;
+                playerDown = true;
+            }
+
+        } else {
+            System.out.println("error in things ");
+        }
+
+
+
+
+        //.out.println("up " + enemyUp + " down " + enemyDown + " right " + enemyRight + " left " + enemyLeft);
+
+        if (enemyVerticleExit) {
+
+            if (enemyUp) {
+                enemyYpointIntersection = (this.getY());
+                enemyXpointIntersection = ((enemyYpointIntersection - b) / m);
+
+            } else if (enemyDown) {
+                enemyYpointIntersection = (this.getY() + this.getHeight());
+                enemyXpointIntersection = ((enemyYpointIntersection - b) / m);
+
+            }
+
+        } else if (enemyHorizontalExit) {
+
+            if (enemyRight) {
+                enemyXpointIntersection = (this.getX() + this.getWidth());
+                enemyYpointIntersection = ((m * enemyXpointIntersection) + b);
+
+            } else if (enemyLeft) {
+                enemyXpointIntersection = (this.getX());
+                enemyYpointIntersection = ((m * enemyXpointIntersection) + b);
+            }
+
+        }
+
+        if (playerVerticleExit) {
+
+            if (playerUp) {
+                playerYpointIntersection = (player.getY());
+                playerXpointIntersection = ((playerYpointIntersection - b) / m);
+
+            } else if (playerDown) {
+                playerYpointIntersection = (player.getY() + player.getHeight());
+                playerXpointIntersection = ((playerYpointIntersection - b) / m);
+
+            }
+
+        } else if (playerHorizontalExit) {
+
+            if (playerRight) {
+                playerXpointIntersection = (player.getX() + player.getWidth());
+                playerYpointIntersection = ((m * playerXpointIntersection) + b);
+            } else if (playerLeft) {
+                playerXpointIntersection = (player.getX());
+                playerYpointIntersection = ((m * playerXpointIntersection) + b);
+            }
+
+        }
+
+        return Math.sqrt( Math.pow((playerXpointIntersection - enemyXpointIntersection), 2) + Math.pow((playerYpointIntersection - enemyYpointIntersection), 2) );
 
     }
 
